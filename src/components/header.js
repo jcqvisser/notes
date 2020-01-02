45463a049334bb3a28bae92ae43be790ebfff3c5
Jacques Visser <jcqvisser@gmail.com>
2020-01-02 14:42:18 +0200

    Add normalizer and do some cleanup

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `hsla(219, 5%, 35%, 1)`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,26 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
+    <dl>
+      <dt>Note Title</dt>
+      <dd>
+        <small><time>2020-01-01</time></small>
+        <p>Note description; a fair amount of text goes here</p>
+      </dd>
+
+      <dt>Another Note Title</dt>
+      <dd>
+        <small><time>2020-01-02</time></small>
+        <p>Note description; a fair amount of text goes here. A whole lot of text could be displayed, what then? what will we do? It will have to break at some point.</p>
+      </dd>
+    </dl>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
