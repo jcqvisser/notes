21789d22570ae1945881099985b22129561af6a8
Jacques Visser <jcqvisser@gmail.com>
2020-01-03 18:26:44 +0200

    remove the link underline from the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 const Header = ({title}) => (
   <header className="container mx-auto flex justify-left py-6">
-    <Link to="/">
+    <Link to="/" className="no-underline">
       <h1 className="font-light text-white bg-gray-900 px-4 text-5xl rounded-sm">{title.toUpperCase()}</h1>
     </Link>
   </header>
