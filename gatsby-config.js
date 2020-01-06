74a150a85eb88c34d5f0187abcf04c6bf501396a
Jacques Visser <jcqvisser@gmail.com>
2020-01-06 09:28:22 +0200

    tell purgecss to ignore the global styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,8 +42,8 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelist: ['pre', 'code', 'h2', 'h3'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // whitelist: ['pre', 'code', 'h2', 'h3'], // Don't remove this selector
+        ignore: ['src/styles/global.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
     }
